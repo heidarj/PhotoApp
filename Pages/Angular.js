739c6ae7cf50aef8c73939ce79178b45ad8ad584
heidarj <heidarj@hraunteigur5.com>
2016-04-19 22:24:52 +0000

    Express
    Basic Express implementation

--- Pages/Angular.js
+++ Pages/Angular.js
@@ -1,5 +1,32 @@
 var app = angular.module('PhotoApp', []);
+
+//index.html view controller
 app.controller('MainView', function($scope) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
+    
+    $scope.posts = [];
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+
+    $scope.post = function(){
+        $scope.newPost.created_at = Date.now();
+        $scope.posts.push($scope.newPost);
+        $scope.newPost = {created_by: '', text: '', created_at: ''};
+    };
+});
+
+//login and registration controller
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
 });
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var funcs = require('./funcs')
 var http = require('http')
 fs = require('fs')
+express = require('express')
 
-http.createServer(function(req, res) {
-    res.writeHeader(200, {"Content-Type": "text/html"}); 
+var app = express();
+
+app.get('/', function(req, res) {
+    
     html = fs.createReadStream('Pages/index.html').pipe(res)
+    
+});
+
+
+
+
+
+
+
+
+app.listen(80);
+
+
+//http.createServer(function(req, res) {
+//    res.writeHeader(200, {"Content-Type": "text/html"}); 
+//    html = fs.createReadStream('Pages/index.html').pipe(res)
     //res.write();  
     //res.end(html);  
-}).listen(80, "10.42.103.25");
+//}).listen(80, "localhost");
+
+
 
 console.log("HTTP server started at ip 10.42.103.25")
